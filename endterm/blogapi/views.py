af5d37b4073eaf937b46489f88f13b84b5294216
Aru <aru.omarali@gmail.com>
2018-04-20 12:31:01 +0600

    enderm

--- endterm/blogapi/views.py
+++ endterm/blogapi/views.py
@@ -9,15 +9,15 @@ from django.shortcuts import redirect
 @csrf_exempt
 def post_list(request):
     if request.method == "GET":
-        posts = Post.objects.all().order_by('priority')
+        posts = Post.objects.all()
         posts_json = [t.to_json() for t in posts]
         return JsonResponse(posts_json, safe=False)
       
     elif request.method == "POST":
         data = request.POST
         post = Post()
+        post.title = data.get('title','')
         post.text = data.get('text','')
-        post.priority = data.get('priority', '')
         post.save()
     
     return JsonResponse(post.to_json(), safe=False)
@@ -25,19 +25,19 @@ def post_list(request):
     
 def post_details(request, post_id):
     post = Post.objects.get(pk=post_id)
-    return render(request, 'post/post_details.html', {'post': post})
+    return render(request, 'blog/post_details.html', {'post': post})
 
 def post_edit(request, pk):
     post = Post.objects.get(pk=pk)
     if request.method == "POST":
-        form = ostForm(request.POST, instance=post)
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.save()
             return redirect('post_list')
     else:
-        form = postForm(instance=post)
-    return render(request, 'post/post_edit.html', {'form': form})
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 
 def post_new(request):
@@ -48,8 +48,8 @@ def post_new(request):
             post.save()
             return redirect('post_list')
     else:
-        form = postForm()
-    return render(request, 'post/post_edit.html', {'form': form})
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_delete(request,pk):
     post = Post.objects.get(id=pk)
